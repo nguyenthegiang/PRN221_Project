f99de4723c5932d7b7934bfb5f05c97dbbedb799
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2022-02-22 10:10:00 +0700

    Add: DAO & functions

---
+++ PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/DAO/CategoryDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using PRN221_Project_ShopOnline.Models;
+
+namespace PRN221_Project_ShopOnline.DAO
+{
+    public class CategoryDAO
+    {
+        public IEnumerable<Category> GetAllroducts()
+        {
+            ElectronicShopPRN221Context context = new ElectronicShopPRN221Context();
+            IEnumerable<Category> categories = context.Categories.ToList();
+            return categories;
+        }
+    }
+}
---
+++ PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/DAO/ProductDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using PRN221_Project_ShopOnline.Models;
+
+namespace PRN221_Project_ShopOnline.DAO
+{
+    public class ProductDAO
+    {
+        public IEnumerable<Product> GetAllroducts()
+        {
+            ElectronicShopPRN221Context context = new ElectronicShopPRN221Context();
+            IEnumerable<Product> products = context.Products.ToList();
+            return products;
+        }
+    }
+}
