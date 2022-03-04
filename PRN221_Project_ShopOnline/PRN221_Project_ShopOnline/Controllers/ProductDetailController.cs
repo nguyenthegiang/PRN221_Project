b5133fd1bfb6873c998dec07850e495cb9726eba
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2022-03-04 21:12:32 +0700

    [Function] View Product Detail: Code Controller

--- PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/Controllers/ProductDetailController.cs
+++ PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/Controllers/ProductDetailController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PRN221_Project_ShopOnline.DAO;
+using PRN221_Project_ShopOnline.Models;
 
 namespace PRN221_Project_ShopOnline.Controllers
 {
     public class ProductDetailController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(int ProductID)
         {
             var view = View("Views/ProductDetail.cshtml");
+
+            //Get product Detail
+            ProductDAO dao = new ProductDAO();
+            Product p = dao.GetProductByID(ProductID);
+
+            //set to ViewBag
+            ViewBag.Product = p;
 
             return view;
         }
