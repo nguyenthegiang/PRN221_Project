ed02c764bf925cc49023267a81df3956f7006a90
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-15 20:29:36 +0700

    [Login]: Create Views & Controller

---
+++ PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/DAO/UserDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using PRN221_Project_ShopOnline.Models;
+
+namespace PRN221_Project_ShopOnline.DAO
+{
+    public class UserDAO
+    {
+        private readonly ElectronicShopPRN221Context context = new ElectronicShopPRN221Context();
+
+    }
+}
