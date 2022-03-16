5f43513cc4e0e4f452ceee9835c8b4cdbd09fac0
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-16 21:08:00 +0700

    [Login] - BackEnd: Set Cookies for auto login

--- PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/Controllers/LoginController.cs
+++ PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/Controllers/LoginController.cs
@@ -4,12 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 
 using PRN221_Project_ShopOnline.DAO;
 using PRN221_Project_ShopOnline.Models;
 
-using System.Web;
+using Microsoft.AspNetCore.Http;
 
 namespace PRN221_Project_ShopOnline.Controllers
 {
@@ -35,9 +34,17 @@ namespace PRN221_Project_ShopOnline.Controllers
             {
                 //login success: 
 
-                //set User to Session
+                //set User to Session (for other pages to use)
                 HttpContext.Session.SetString("username", user.Username);
+                HttpContext.Session.SetString("password", user.Password);
                 HttpContext.Session.SetInt32("userId", user.UserId);
+
+                //set User to Cookies (for auto Login)
+                if (!HttpContext.Request.Cookies.ContainsKey("userId"))
+                {
+                    HttpContext.Response.Cookies.Append("username", user.Username);
+                    HttpContext.Response.Cookies.Append("password", user.Password);
+                }
 
                 //to Home Page
                 return Redirect("/");
