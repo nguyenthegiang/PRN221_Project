83ddcc92c6e24975582b897bab921eb54fdbb09a
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-22 23:14:00 +0700

    [ManageProduct-List]: Display Data (Controller)

--- PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/Controllers/ProductManagerController.cs
+++ PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/Controllers/ProductManagerController.cs
@@ -4,12 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using PRN221_Project_ShopOnline.Models;
+using PRN221_Project_ShopOnline.DAO;
+using Microsoft.AspNetCore.Http;
+
 namespace PRN221_Project_ShopOnline.Controllers
 {
     public class ProductManagerController : Controller
     {
         public IActionResult Index()
         {
+            //Get info of this Seller from Session
+            int sellerId = (int) HttpContext.Session.GetInt32("userId");
+            string sellerName = HttpContext.Session.GetString("username");
+
+            //Get list products of this Seller for Manage
+            ProductDAO productDAO = new ProductDAO();
+            List<Product> products = productDAO.GetProductsBySeller(sellerId).ToList();
+
+            //Set data to View
+            ViewBag.Products = products;
+
             return View("Views/ProductManager.cshtml");
         }
     }
