fe25801d92ad4bee2b4cb51f9bd8d9c8df5b8789
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-24 16:08:26 +0700

    [ManageAccount-List]: Route

---
+++ PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/Controllers/AccountManagerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRN221_Project_ShopOnline.Controllers
+{
+    public class AccountManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
