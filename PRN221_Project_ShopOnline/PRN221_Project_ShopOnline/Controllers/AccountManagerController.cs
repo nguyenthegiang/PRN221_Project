fd14c97ae0f5790b898af84c16d93d671d02fdb9
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-24 16:14:35 +0700

    [ManageAccount-List]: Controller

--- PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/Controllers/AccountManagerController.cs
+++ PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/Controllers/AccountManagerController.cs
@@ -4,13 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using PRN221_Project_ShopOnline.DAO;
+using PRN221_Project_ShopOnline.Models;
+
 namespace PRN221_Project_ShopOnline.Controllers
 {
     public class AccountManagerController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            //Get data from DB
+            UserDAO dao = new UserDAO();
+            List<User> users = dao.GetAllAccounts().ToList();
+
+            //Set data to View
+            ViewBag.Users = users;
+
+            return View("Views/AccountManager.cshtml");
         }
     }
 }
