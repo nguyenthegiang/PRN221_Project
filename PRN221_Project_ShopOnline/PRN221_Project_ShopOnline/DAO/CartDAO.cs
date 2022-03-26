9c8cfbf457fd7b9911889a9c9e217edd14a380a7
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-26 12:43:47 +0700

    [Cart-AddToCart]: DAO {getCartsOfUser}

--- PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/DAO/CartDAO.cs
+++ PRN221_Project_ShopOnline/PRN221_Project_ShopOnline/DAO/CartDAO.cs
@@ -13,6 +13,13 @@ namespace PRN221_Project_ShopOnline.DAO
         private readonly ElectronicShopPRN221Context context = new ElectronicShopPRN221Context();
 
         /*-------------Customer-------------*/
+        //Get all Cart Items of 1 User
+        public List<Cart> GetCartsOfUser(int UserId)
+        {
+            List<Cart> carts = context.Carts.Where(c => c.UserId == UserId).ToList();
+            return carts;
+        }
+
         //Count the amount of a product to see if it is out of stock: used for AddToCart()
         public int countAmountOfProduct(int ProductId)
         {
